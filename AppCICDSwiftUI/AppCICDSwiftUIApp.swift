85303297e2deb945e537b4f0d69b9fb1cef3403a
Alexander Cayetano <sr.alexco@gmail.com>
2025-04-09 12:45:42 -0500

    First Commit

---
+++ AppCICDSwiftUI/AppCICDSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCICDSwiftUIApp.swift
+//  AppCICDSwiftUI
+//
+//  Created by alex on 9/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AppCICDSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCICDSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppCICDSwiftUI
+//
+//  Created by alex on 9/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
