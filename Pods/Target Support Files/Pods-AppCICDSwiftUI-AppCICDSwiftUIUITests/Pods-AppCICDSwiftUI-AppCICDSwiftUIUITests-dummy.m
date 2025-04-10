93b78258a49ea8174d999f2081a8325e721ca3fa
Alexander Cayetano <sr.alexco@gmail.com>
2025-04-09 21:10:38 -0500

    Added Basic Configurations

---
+++ Pods/Target Support Files/Pods-AppCICDSwiftUI-AppCICDSwiftUIUITests/Pods-AppCICDSwiftUI-AppCICDSwiftUIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppCICDSwiftUI_AppCICDSwiftUIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppCICDSwiftUI_AppCICDSwiftUIUITests
+@end
---
+++ Pods/Target Support Files/Pods-AppCICDSwiftUITests/Pods-AppCICDSwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppCICDSwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppCICDSwiftUITests
+@end
