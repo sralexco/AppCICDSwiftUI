f11d716b8cfd30c3494afddc10c4ae1d5ed62dac
Alexander Cayetano <sr.alexco@gmail.com>
2025-04-10 02:44:31 -0500

    Change to color blue

--- AppCICDSwiftUI/ContentView.swift
+++ AppCICDSwiftUI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("New Color redd!")
+            Text("New Color blue!")
         }
         .padding()
     }
