5ca488789dcb04e94a6fc847409e019ba4b57fbf
Alexander Cayetano <sr.alexco@gmail.com>
2025-04-10 03:15:43 -0500

    Adding color Yellow

--- AppCICDSwiftUI/ContentView.swift
+++ AppCICDSwiftUI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("New Color blue!")
+            Text("New Color yellow!")
         }
         .padding()
     }
